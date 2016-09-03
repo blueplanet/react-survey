4a2eee2fb7e44071a9f3e4a78aa014dfb0ba03f5
blueplanet <erguolinge@gmail.com>
2016-09-04 07:26:38 +0900

    Add suvery:wq

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822215503) do
+ActiveRecord::Schema.define(version: 20160903222415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "surveys", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
