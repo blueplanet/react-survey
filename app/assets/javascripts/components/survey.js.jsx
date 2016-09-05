40c896ae12aa54a65679044a08d76af7782a9670
blueplanet <erguolinge@gmail.com>
2016-09-05 09:09:34 +0900

    surveys#show

--- app/assets/javascripts/components/survey.js.jsx
+++ app/assets/javascripts/components/survey.js.jsx
@@ -2,6 +2,9 @@ var Survey = React.createClass({
 
   render: function() {
     return (
+      <ReactBootstrap.Panel header={this.props.name}>
+        {this.props.description}
+      </ReactBootstrap.Panel>
     );
   }
 });
